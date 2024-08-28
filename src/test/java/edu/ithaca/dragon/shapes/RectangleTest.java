dd7c33b981201dfb4f60107179df0e560d535d5d
harrison-spangler <harrison.spangler@gmail.com>
2024-08-28 19:39:12 -0400

    added tests for longestlinewithin

--- src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
@@ -30,4 +30,15 @@ public class RectangleTest {
         Rectangle myRectangle2 = new Rectangle(10, 4);
         assertEquals(80, myRectangle2.calcArea());
     }
+    @Test
+    public void longestLineWithinTest(){
+        Rectangle myRectangle = new Rectangle(3, 2);
+        assertEquals(3, myRectangle.longestLineWithin());
+        
+        Rectangle myRectangle1 = new Rectangle(4, 2);
+        assertEquals(4, myRectangle1.longestLineWithin());
+
+        Rectangle myRectangle2 = new Rectangle(10, 4);
+        assertEquals(10, myRectangle2.longestLineWithin());
+    }
 }
